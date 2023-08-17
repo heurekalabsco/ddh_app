fe703053f975dfe5650046c8cffbc6a13fd0a894
Matthew Hirschey <matthew@hirschey.org>
2023-08-17 17:54:15 -0400

    adds APP dir
    this commit moves all code to a new APP dir, and adds an index.html file for each of the versions.

---
+++ code/app/private.R
@@ -0,0 +1,14 @@
+private <- function(x, publicValue=NULL) {
+  if (privateMode == TRUE) { x } else { publicValue }
+}
+
+private_message_html <- "This analysis is currently in Beta, and is only available to subscribers. If you want to upgrade to support this project and access these data, subscribe at http://www.datadrivenhypothesis.com."
+private_message_md <- "This analysis is currently in Beta, and is only available to subscribers. If you want to upgrade to support this project and access these data, subscribe at [datadrivenhypothesis.com](http://www.datadrivenhypothesis.com)"
+
+private_serachbox <- "genes, cells, or compounds"
+
+private_msg <- function() {
+  if (privateMode == FALSE) {
+    htmltools::tagList(private_message_html) 
+  }
+}
---
+++ code/app/www/outseta.js
@@ -0,0 +1,20 @@
+<!-- Login Embed -->
+<script>
+  var o_options = {
+    domain: 'ddh.outseta.com'
+  };
+</script>
+<script src="https://cdn.outseta.com/outseta.min.js"
+        data-options="o_options">
+</script>
+
+<!-- Chat Embed -->
+<script>
+  var o_options = {
+    domain: 'ddh.outseta.com',
+    load: 'chat'
+  };
+</script>
+<script src="https://cdn.outseta.com/outseta.min.js"
+        data-options="o_options">
+</script>
