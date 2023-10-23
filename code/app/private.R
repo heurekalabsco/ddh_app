4d59508a56e334f7ba8a7e0a4847cf7879b29b8d
Matthew Hirschey <matthew@hirschey.org>
2023-10-23 16:24:44 -0400

    updates private message

--- code/app/private.R
+++ code/app/private.R
@@ -2,13 +2,13 @@ private <- function(x, publicValue=NULL) {
   if (privateMode == TRUE) { x } else { publicValue }
 }
 
-private_message_html <- "This analysis is currently in Beta, and is only available to subscribers. If you want to upgrade to support this project and receive access, subscribe at <a href='https://www.datadrivenhypothesis.com' target='_blank'>www.datadrivenhypothesis.com."
-private_message_md <- "This analysis is currently in Beta, and is only available to subscribers. If you want to upgrade to support this project and receive access, subscribe at [datadrivenhypothesis.com](https://www.datadrivenhypothesis.com)"
+private_message_html <- "This analysis is only available to premium subscribers. If you want to subscribe to support this project and receive access, go to <a href='https://www.datadrivenhypothesis.com' target='_blank'>www.datadrivenhypothesis.com</a>."
+private_message_md <- "This analysis is only available to premium subscribers. If you want to subscribe to support this project and receive access, go to [datadrivenhypothesis.com](https://www.datadrivenhypothesis.com)."
 
 private_searchbox <- "genes, cells, or compounds"
 
 private_msg <- function() {
   if (privateMode == FALSE) {
-    htmltools::tagList(private_message_html) 
+    shiny::HTML(private_message_html) 
   }
 }
