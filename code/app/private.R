9e38f3a5a05ea889bee6fed92a51bccdae221868
pcastellanoescuder <polcaes@gmail.com>
2024-01-26 11:59:00 -0600

    update private message

--- code/app/private.R
+++ code/app/private.R
@@ -2,8 +2,8 @@ private <- function(x, publicValue=NULL) {
   if (privateMode == TRUE) { x } else { publicValue }
 }
 
-private_message_html <- "This analysis is only available to premium subscribers. If you want to subscribe to support this project and receive access, go to <a href='https://www.datadrivenhypothesis.com' target='_blank'>www.datadrivenhypothesis.com</a>."
-private_message_md <- "This analysis is only available to premium subscribers. If you want to subscribe to support this project and receive access, go to [datadrivenhypothesis.com](https://www.datadrivenhypothesis.com)."
+private_message_html <- "This feature is only available to premium subscribers. If you want to subscribe to support this project and receive access, go to <a href='https://www.datadrivenhypothesis.com' target='_blank'>www.datadrivenhypothesis.com</a>."
+private_message_md <- "This feature is only available to premium subscribers. If you want to subscribe to support this project and receive access, go to [datadrivenhypothesis.com](https://www.datadrivenhypothesis.com)."
 
 private_searchbox <- "genes, cells, or compounds"
 
